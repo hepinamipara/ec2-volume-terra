94aa6e6b83413e4a4e6f3ca03ba5f41d5fa0afdb
hepinamipara <113300990+hepinamipara@users.noreply.github.com>
2025-12-10 12:48:22 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -10,35 +10,42 @@ variable "ami" {
   default     = "ami-0c398cb65a93047f2"
 }
 
+
+variable "key_name" {
+  type        = string
+  description = "Enter your key pair name to enable SSH access"
+  default     = "linux"
+}
+
 variable "vpc_id" {
   type        = string
   description = "Your VPC ID (used for tagging / reference)"
-  default     = ""
+  default     = "vpc-0349b32f0c9ad99aa"
 }
 
 variable "subnet_id" {
   type        = string
   description = "Subnet ID where EC2 will be launched (required)"
-  default     = ""
+  default     = "subnet-03e5def87757b3f6b"
 
 }
 
 variable "security_group_ids" {
   type        = list(string)
   description = "List of security group IDs to attach to the instance"
-  default     = []
+  default     = ["sg-0166cb1d8a6fc4847"]
 }
 
 variable "availability_zone" {
   type        = string
   description = "Availability Zone (used for extra EBS volumes' AZ)"
-  default     = ""
+  default     = "us-east-1a"
 }
 
 variable "root_volume_size" {
   type        = number
   description = "Root volume size (GB)"
-  default     = 10
+  default     = 8
 }
 
 variable "volume1_size" {
